8ffc093c96ff1ec4a9c7f0f7c56a0e065095afde
LeonardMarian <118011483+LeonardMarian@users.noreply.github.com>
2022-12-13 08:20:03 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,11 @@
+#include "lib.h"
+float numeromaggiore( float numeri[], int a){
+    for (int i=0; i < a; i++){
+        if( numeri[0] > numeri[i]){
 
+        }else {
+            numeri[0]= numeri [i];
+        }
+    }
+    return numeri[0];
+}
